7a78d973fb4c0d69cab152693eab1b4329cd4d2c
Daniel Arenas <darenas@MacBook-Air-de-Daniel.local>
2019-10-22 17:09:51 -0500

    added validation to count reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,10 @@
 class Reservation < ApplicationRecord
   belongs_to :movie
+  validate :count_reservation
+
+  private
+
+  def count_reservation
+    errors.add(:count_reservations, "can't be greater than 10") if movie.reservations.count > 10
+  end
 end
